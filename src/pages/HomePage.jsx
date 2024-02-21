1391c24ccb3222b6e3258a84569ddea0623e254e
alishaker22033 <160706898+alishaker22033@users.noreply.github.com>
2024-02-21 07:37:46 +0000

    created mainlayout

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MainLayout from '../components/MainLayout'
+
+const HomePage = () => {
+  return (
+    <MainLayout>
+      <p>this is the main page</p>  
+    </MainLayout>
+  )
+}
+
+export default HomePage
