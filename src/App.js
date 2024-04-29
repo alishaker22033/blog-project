e0a22effe995c7fc82d2c674c5df86b2ecccb08a
alishaker22033 <160706898+alishaker22033@users.noreply.github.com>
2024-04-29 09:42:35 +0000

    Responsive Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import './App.css';
-import HomePage from './pages/HomePage';
+import "./App.css";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="App">
-     <HomePage/>
-    </div >
+    <div className="App font-opensans">
+      <HomePage />
+    </div>
   );
 }
-
 export default App;
